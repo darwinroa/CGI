5ec1563b4ff7fa189ada0e1cffd8b9e2bc99a11e
Darwin Roa <darwinmichaelroamora@gmail.com>
2023-05-05 19:24:45 -0500

    Ready footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import * as style from "./Footer.module.css";
 
 export default function index() {
   return (
-    <footer>
-      <div className="container">
-        <p className="copyright">(C) 2023 CGI CZ s.r.o.</p>
+    <footer className={style.footer}>
+      <div className={`container ${style.container}`}>
+        <p className={style.copyright}>(C) 2023 CGI CZ s.r.o.</p>
       </div>
     </footer>
   );
