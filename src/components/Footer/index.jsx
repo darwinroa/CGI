d9aaad2d604d6d35e310bf56f19f56ca8120a041
Darwin Roa <darwinmichaelroamora@gmail.com>
2023-05-31 19:36:40 -0500

    The Year in the footer is now dynamic

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import * as style from "./Footer.module.css";
 
 export default function index() {
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const year = new Date().getFullYear();
+      document.getElementById("year").textContent = year;
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <footer className={style.footer}>
       <div className={`container ${style.container}`}>
-        <p className="copyright">(C) 2023 CGI CZ s.r.o.</p>
+        <p className="copyright">
+          (C) <span id="year"></span> CGI CZ s.r.o.
+        </p>
       </div>
     </footer>
   );
